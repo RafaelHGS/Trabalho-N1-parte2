f4f6ad1cc2464a9412866d5c996c7c3c9d403de0
RafaelHGS <89417905+RafaelHGS@users.noreply.github.com>
2021-10-08 10:27:42 -0300

    Interface grafica
    Interface gráfica embutida no terminal para melhor usabilidade do usuário

--- TrabalhoN1-Parte2.py
+++ TrabalhoN1-Parte2.py
@@ -1,6 +1,20 @@
+#Definindo as Funções:
+
+def telaDeSelecao():
+    print("Seleciona uma opção:")
+    print("[1] Cadastrar Novo Usuário")
+    print("[2] Listar Usuários.")
+    print("[3] Listar Usuários, por ordem alfabética de acordo com o Nome de Usuário")
+    print("[4] Remover Usuário")
+    print("[5] Alterar Nome de Usuário.") 
+    print("[6] Para encerrar o programa")
+
+#Main do Programa
 def main():
     opcaoDoPrograma = 0
     while(opcaoDoPrograma >= 0 and opcaoDoPrograma <= 5):   #Laço de Repetição Principal da função
+        telaDeSelecao()
+
         if(opcaoDoPrograma == 1):
             print("1")
         elif(opcaoDoPrograma == 2):

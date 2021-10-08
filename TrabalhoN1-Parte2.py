d95a840e09574a7412d1218f9ff9ed4a229ffe99
RafaelHGS <89417905+RafaelHGS@users.noreply.github.com>
2021-10-08 10:23:55 -0300

    Main
    Definindo escopo do programa

---
+++ TrabalhoN1-Parte2.py
@@ -0,0 +1,17 @@
+def main():
+    opcaoDoPrograma = 0
+    while(opcaoDoPrograma >= 0 and opcaoDoPrograma <= 5):   #Laço de Repetição Principal da função
+        if(opcaoDoPrograma == 1):
+            print("1")
+        elif(opcaoDoPrograma == 2):
+            print("2")
+        elif(opcaoDoPrograma == 3):
+            print("3")
+        elif(opcaoDoPrograma == 4):
+            print("4")
+        elif(opcaoDoPrograma == 5):
+            print("5")
+    print("-------//-Fim da Aplicação-//-------") 
+
+if __name__ == "__main__":
+    main()

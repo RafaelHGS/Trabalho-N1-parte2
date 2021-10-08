58aa8ab150c029a1dfff526d822eeffbe61fd560
Guss <48757552+GussPlays@users.noreply.github.com>
2021-10-08 11:16:56 -0300

    Update TrabalhoN1-Parte2.py
    Remover e alterar usuário

--- TrabalhoN1-Parte2.py
+++ TrabalhoN1-Parte2.py
@@ -8,7 +8,7 @@ def telaDeSelecao():
     print("Seleciona uma opção:")
     print("[1] Cadastrar Novo Usuário")
     print("[2] Listar Usuários.")
-    print("[3] Listar Usuários, por ordem alfabética de acordo com o Nome de Usuário")
+    print("[3] Listar Usuários por Nome de Usuário")
     print("[4] Remover Usuário")
     print("[5] Alterar Nome de Usuário.") 
     print("[6] Para encerrar o programa")
@@ -29,6 +29,40 @@ def verificandoUsuario(nome):   #H4.: Como gestor do sistema gostaria de verific
         if nome == usuario["nome"]:
             print(f'Nome do Usuário: {usuario["nome"]} \t//\t E-mail: {usuario["e-mail"]}')
 
+def removendoUsuario(email):   #H5.: Como gestor do sistema gostaria poder remover um usuário cadastrado, buscando-o por seu e-mail.
+    for buscandoUsuario in usuariosCadastrados:
+        if email == buscandoUsuario["e-mail"]:
+            usuariosCadastrados.remove(buscandoUsuario)
+    for buscandoUsuario in usuariosEmAlfabetico:
+        string = buscandoUsuario.split(" ")
+        if email == string[6]: 
+            usuariosEmAlfabetico.remove(buscandoUsuario)
+            print(f'---------O Usuário de Email "{email}" foi removido---------')
+            print("\n-----------------------///-----------------------\n")
+            return
+    print("\n---------// Usuário Não Encontrado !!! //---------\n")
+    print("\n-----------------------///-----------------------\n")
+
+
+def alterarUsuario(email):   #H6.: Como gestor do sistema gostaria de poder alterar o nome de um usuário cadastrado no sistema, buscando-o por seu e-mail.
+    for localiza in usuariosCadastrados:
+        if email == localiza["e-mail"]:
+            novoNome = input("Digite o novo nome do Usuario: ")
+            localiza["nome"] = novoNome
+    for localiza in usuariosEmAlfabetico:
+        palavraSeparada = localiza.split(" ")
+        if email == palavraSeparada[6]:
+            usuariosEmAlfabetico.remove(localiza)
+            novoNomeDeUsuario = palavraSeparada
+            novoNomeDeUsuario[3] = novoNome
+            novoNomeDeUsuario = "Nome do Usuário: " + novoNomeDeUsuario[3] + " \t//\t E-mail: " + novoNomeDeUsuario[6]
+            usuariosEmAlfabetico.append(novoNomeDeUsuario)
+            print(f'---------O nome de usuário de Email "{email}" foi alterado para {novoNome}---------')
+            print("\n-----------------------///-----------------------\n")
+            return
+    print("\n---------// Usuário Não Encontrado !!! //---------\n")
+    print("\n-----------------------///-----------------------\n")
+           
 #Main do Programa
 def main():
     opcaoDoPrograma = 0
